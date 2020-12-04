c075b3c3b7d0dd8b529c7edfdf84a4c7a868ce03
Volodymyr Lapan <vlapan@zoolatech.com>
2020-12-04 14:09:29 +0200

    Change gemfile next path contstant

--- lib/gemfile_next_auto_sync.rb
+++ lib/gemfile_next_auto_sync.rb
@@ -5,8 +5,8 @@ require_relative 'gemfile_next_auto_sync/version'
 module GemfileNextAutoSync
   GEMFILE = ::Bundler.default_gemfile
   GEMFILE_LOCK = Pathname("#{GEMFILE}.lock")
-  GEMFILE_NEXT_LOCK = Pathname("#{GEMFILE}_next.lock")
-
+  GEMFILE_NEXT_LOCK = Pathname("#{GEMFILE}.next.lock")
+  Gemfile.next.lock
   autoload :Synchronizer, 'gemfile_next_auto_sync/synchronizer'
 end
 
