a81d77d5cebd5cb1766268520a8a6455345ef84c
Volodymyr Lapan <vlapan@zoolatech.com>
2020-12-09 14:38:18 +0200

    Remove extras

--- lib/gemfile_next_auto_sync.rb
+++ lib/gemfile_next_auto_sync.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'gemfile_next_auto_sync/version'
-require_relative "gemfile_next_auto_sync/bundler_patch"
 
 module GemfileNextAutoSync
   autoload :Synchronizer, 'gemfile_next_auto_sync/synchronizer'
