3df0ce9efe04f60710fd962163ffe2986a11aab9
Volodymyr Lapan <vlapan@zoolatech.com>
2020-12-09 18:34:59 +0200

    Remove comments

--- lib/gemfile_next_auto_sync/synchronizer.rb
+++ lib/gemfile_next_auto_sync/synchronizer.rb
@@ -54,13 +54,6 @@ module GemfileNextAutoSync
 
       next_definition = Bundler::Definition.build(GEMFILE_NEXT, GEMFILE_NEXT_LOCK, unlock)
       definition = Bundler::Definition.build(GEMFILE, lock, unlock)
-      # next_dependencies = File.read(GEMFILE_NEXT).scan(%r{(gem)(\s+)(?:'|")([^'"]*)(?:'|")(?:(?:,\s+)?(?:'|")?([^'"\n]*)?(?:'|"))?}).map do |x|
-      #   Bundler::Dependency.new(x[2], x[3])
-      # end
-
-      # next_dependencies_names = next_dependencies.map{ |dep| dep.name }
-      # definition.dependencies.reject! { |d| next_dependencies_names.include?(d.name) }
-      # definition.dependencies.prepend(*next_dependencies)
       definition.dependencies.reject!{ true }
       definition.dependencies.append(*next_definition.dependencies)
       definition.resolve_remotely!
