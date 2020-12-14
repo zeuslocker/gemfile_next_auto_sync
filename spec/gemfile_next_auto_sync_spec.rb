2ab2587c74464059f5a04b0384dd898d42c56252
Volodymyr Lapan <vlapan@zoolatech.com>
2020-12-14 13:23:37 +0200

    Update

--- spec/gemfile_next_auto_sync_spec.rb
+++ spec/gemfile_next_auto_sync_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe GemfileNextAutoSync do
     expect(File.read('spec/fixtures/Gemfile.lock')).to include 'catalogue (0.0.1)'
     expect(File.read('spec/fixtures/Gemfile.next.lock')).to include 'catalogue (0.0.1)'
   end
+
+  it 'Removes shared gem from both gemfiles' do
+    original = File.read('spec/fixtures/Gemfile.shared')
+    File.write('spec/fixtures/Gemfile.shared', original.gsub("gem 'catalogue', '0.0.1'", ''), mode: 'w')
+    subject
+    expect(File.read('spec/fixtures/Gemfile.lock')).not_to include 'catalogue (0.0.1)'
+    expect(File.read('spec/fixtures/Gemfile.next.lock')).not_to include 'catalogue (0.0.1)'
+  ensure
+    File.write('spec/fixtures/Gemfile.shared', original, mode: 'w')
+  end
 end
